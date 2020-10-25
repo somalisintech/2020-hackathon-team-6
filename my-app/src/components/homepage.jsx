5a8742db75c1f1f388e21b89a37d11b80b91cb60
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-10-25 09:25:02 +0000

    Added flow ai chatbot to homepage

--- my-app/src/components/homepage.jsx
+++ my-app/src/components/homepage.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { LandingPage } from './landingPage';
 import { MidContent } from './mid-content-1';
 import { MidContent2 } from './mid-content-2';
 import { MidContent3 } from './mid-content-3';
 
 export const HomePage = () => {
+
+    useEffect(() => {
+        const script = document.createElement('script');
+
+        script.src = "https://widget.flowai.app/w/YTZUeldSMjRkfGNzV2VXZjc4aA==/flow-webclient-1.1.2.min.js"
+        script.async = true 
+
+        document.body.appendChild(script);
+
+        return () => {
+            document.body.removeChild(script);
+        }
+    })
     return (
         <>
             <LandingPage/>
