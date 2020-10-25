516144fad246375b0669b810a62a0080d089a485
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-10-25 13:35:54 +0000

    Removed logo from footer and made lighter grey

--- my-app/src/components/footer.jsx
+++ my-app/src/components/footer.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import styled from "styled-components";
-import cdLogo from '../img/cd-logo.svg';
+// import cdLogo from '../img/cd-logo.svg';
 
 const FooterContainer = styled.div`
-    height: auto;
+    height: 50px;
     width: 100%;
-    background: #575858;
+    background: #878B8B;
 `;
 
-const LogoContainer = styled.div`
-    width: auto;
-`;
+// const LogoContainer = styled.div`
+//     width: auto;
+// `;
 
 const CopyrightText = styled.div`
     font-style: normal;
@@ -23,16 +23,19 @@ const FlexContainer = styled.div`
     display: flex;
     flex-direction: column;
     margin: 0px 400px;
+    align-items: center;
+    height: 100%;
+    justify-content: center;
 `;
 
 export const Footer = () => {
     return (
         <FooterContainer>
             <FlexContainer>
-                <LogoContainer>
-                    <img src={cdLogo} alt="Caafimaad Direct Logo"></img>
-                </LogoContainer>
-                <CopyrightText>© 2020 Caafimaad Direct</CopyrightText>
+                {/* <LogoContainer>
+                    <img src={cdLogo} alt="Caafimaad Direct Logo" style={{width: "300px"}}></img>
+                </LogoContainer> */}
+                <CopyrightText>© 2020 Caafimaad Direct. All Rights Reserved.</CopyrightText>
               </FlexContainer>
         </FooterContainer>
     )
